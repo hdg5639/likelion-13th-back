5578472c4c19df1c13c9911278e7447c7ced97cb
donggyun-han <hdg5639@daum.net>
2025-05-12 00:25:39 +0900

    feat(Todo): 프론트 1팀 용 todo 서버 2트

--- todo_demo/src/main/java/com/likelion/todo_demo/Config/SecurityConfig.java
+++ todo_demo/src/main/java/com/likelion/todo_demo/Config/SecurityConfig.java
@@ -1,63 +1,63 @@
-package com.likelion.todo_demo.Config;
-
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
-
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests
-                                // GET 요청 허용
-                                .requestMatchers(HttpMethod.GET,
-                                        "/v1/todo")
-                                .permitAll()
-
-                                // POST 요청 허용
-                                .requestMatchers(HttpMethod.POST,
-                                        "/v1/todo")
-                                .permitAll()
-
-                                // DELETE 요청 허용
-                                .requestMatchers(HttpMethod.DELETE,
-                                        "/v1/todo")
-                                .permitAll()
-
-                                // 그 외의 요청은 인증 필요
-                                .anyRequest().authenticated()
-                )
-                .csrf(AbstractHttpConfigurer::disable); // CSRF 비활성화
-        return http.build();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedOriginPattern("*");
-        configuration.setAllowCredentials(true);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-}
+//package com.likelion.todo_demo.Config;
+//
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//
+//import java.util.Arrays;
+//import java.util.List;
+//
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(authorizeRequests ->
+//                        authorizeRequests
+//                                // GET 요청 허용
+//                                .requestMatchers(HttpMethod.GET,
+//                                        "/v1/todo")
+//                                .permitAll()
+//
+//                                // POST 요청 허용
+//                                .requestMatchers(HttpMethod.POST,
+//                                        "/v1/todo")
+//                                .permitAll()
+//
+//                                // DELETE 요청 허용
+//                                .requestMatchers(HttpMethod.DELETE,
+//                                        "/v1/todo")
+//                                .permitAll()
+//
+//                                // 그 외의 요청은 인증 필요
+//                                .anyRequest().authenticated()
+//                )
+//                .csrf(AbstractHttpConfigurer::disable); // CSRF 비활성화
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://127.0.0.1:3000"));
+//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+//        configuration.addAllowedHeader("*");
+//        configuration.addAllowedOriginPattern("*");
+//        configuration.setAllowCredentials(true);
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
+//}
