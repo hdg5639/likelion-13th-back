4e615db7e44dbb13097bf57864529dd4b35eff19
donggyun-han <hdg5639@daum.net>
2025-05-11 17:32:05 +0900

    feat(Todo): 첫 커밋

---
+++ front-todo/front-todo/src/main/java/com/likelion/fronttodo/FrontTodoApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.fronttodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontTodoApplication.class, args);
+    }
+
+}
